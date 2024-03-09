f05bb2ca9db83459afd7bdc9cf23a2918cc63de0
bekah <bekahritter@gmail.com>
2024-03-09 14:52:40 -0800

    fix some errors but not all

--- froyo.js
+++ froyo.js
@@ -1,26 +1,27 @@
 let greeting = prompt("What flavor would you like?",
-    'Vanilla, vanilla, vanilla, strawberry, coffee, coffee');
+    'vanilla, vanilla, vanilla, strawberry, coffee, coffee');
 // START HERE: turn string into array with .split method: take a string.split(what charactor to split ",") //
 // join turns an array into a string?
 
 
 let userOrder = greeting.split(",");
+
 console.log(userOrder);
 //flavors is vanila,strawberry, coffee, how to condense and keep count?
 //OF turns the index to the property, IN turns the index to the value
 
 
-function addUp(order) {
+function addUp() {
     const froyo = {};
-    for (const order of userOrder) {
-        froyo[order] = userOrder[order];
-        if (froyo.order = userOrder[order]) {
-            froyo.order++
-        } else {
-            froyo(order) += 1
-        }
-    } return addUp(userOrder)
+    for (let index = 0; index < userOrder.length; index++) {
+        const flavor = userOrder[index];
 
+        if (froyo[flavor]) {
+            froyo[flavor] += 1;
+        } else {
+            froyo[flavor] = 1;
+        }
+    } return froyo;
 }
 
 // flavors are added to OBJ, but now how to quantify?//
